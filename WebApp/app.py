2a9176058e8af30b203c419ecf396f1f0abea924
Joel Asper <asperj@student.ethz.ch>
2021-12-08 10:44:44 +0100

    added graphs

--- WebApp/app.py
+++ WebApp/app.py
@@ -5,7 +5,7 @@ app.config["DEBUG"] = True
 
 @app.route("/")
 def home():
-    data = [
+    data_pressure = [
         ("11:00",12),
         ("12:00",12.2),
         ("13:00",14),
@@ -16,14 +16,15 @@ def home():
         ("18:00",14.8),
         ("19:00",14.2)
     ]
-    labels = [row[0] for row in data]
-    values = [row[1] for row in data]
 
-    return render_template("index.html", labels=labels, values=values)
+    labels = [row[0] for row in data_pressure]
+    pressure = [row[1] for row in data_pressure]
+    
+    return render_template("index.html", labels=labels, pressure=pressure)
 
 @app.route("/line")
 def line():
-    data = [
+    data_temperature = [
         ("11:00",12),
         ("12:00",12.2),
         ("13:00",14),
@@ -34,10 +35,39 @@ def line():
         ("18:00",14.8),
         ("19:00",14.2)
     ]
-    labels = [row[0] for row in data]
-    values = [row[1] for row in data]
 
-    return render_template("graph.html", labels=labels, values=values)
+    data_humidity = [
+        ("11:00",13),
+        ("12:00",11.2),
+        ("13:00",13),
+        ("14:00",13.2),
+        ("15:00",13.6),
+        ("16:00",12.7),
+        ("17:00",12.2),
+        ("18:00",14.0),
+        ("19:00",14.1)
+    ]
+
+    data_pressure = [
+        ("11:00",12),
+        ("12:00",12.2),
+        ("13:00",14),
+        ("14:00",14),
+        ("15:00",13.6),
+        ("16:00",13.1),
+        ("17:00",12.9),
+        ("18:00",14.8),
+        ("19:00",14.2)
+    ]
+
+    labels_t = [row[0] for row in data_temperature]
+    temperature = [row[1] for row in data_temperature]
+    humidity = [row[1] for row in data_humidity]
+
+    labels_p = [row[0] for row in data_pressure]
+    pressure = [row[1] for row in data_pressure]
+
+    return render_template("graph.html", labels_t=labels_t, temperature=temperature, humidity=humidity, labels_p=labels_p, pressure=pressure)
 
 
 if __name__ == '__main__':
