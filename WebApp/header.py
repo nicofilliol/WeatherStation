a7deac7707936e554c6beed5cde2b75b4d2c9ce5
nicofilliol <nicolas.filliol@outlook.com>
2021-12-12 15:11:58 +0100

    use cloudmqtt in python script

--- WebApp/header.py
+++ WebApp/header.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_mqtt import Mqtt
 from collections import deque
+import configparser
 
 # Helper class for flag
 class Ready_Flag():
@@ -16,9 +17,6 @@ class Ready_Flag():
     def set_state(self, state):
         self.state = state
 
-# Defines
-SUBSCRIPTION_TOPIC = "ethz/weather_station"
-
 # Global variables
 humidity_queue = deque(maxlen=500)
 temperature_queue = deque(maxlen=500)
@@ -27,13 +25,22 @@ light_queue = deque(maxlen=500)
 pressure_queue = deque(maxlen=500)
 new_data_flag = Ready_Flag(False)
 
+# Setup Flask and MQTT Client
+config = configparser.ConfigParser()
+config.read('WebApp/mqtt.config')
+mqtt_details = dict(config.items('MQTT'))
+
 app = Flask(__name__)
-app.config["DEBUG"] = True
-app.config['MQTT_BROKER_URL'] = 'broker.hivemq.com' 
-app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
-app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
-app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
+app.config['DEBUG'] = True
+app.config['MQTT_BROKER_URL'] = mqtt_details['ip_address']
+app.config['MQTT_BROKER_PORT'] = int(mqtt_details['port'])  # default port for non-tls connection
+app.config['MQTT_USERNAME'] = mqtt_details['username']  # set the username here if you need authentication for the broker
+app.config['MQTT_PASSWORD'] = mqtt_details['password']   # set the password here if the broker demands authentication
 app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
 app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+
+# Defines
+SUBSCRIPTION_TOPIC = mqtt_details['subscribe_topic']
+PUBLISH_TOPIC = mqtt_details['publish_topic']
 
 mqtt = Mqtt()
