b8fa3a71b3c6eb6d51a57d62b9e36193ec8c4c34
nicofilliol <nicolas.filliol@outlook.com>
2021-12-15 11:31:01 +0100

    fixed for heroku deployment

--- WebApp/header.py
+++ WebApp/header.py
@@ -21,7 +21,9 @@ class Ready_Flag():
 new_data_flag = Ready_Flag(True)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+database_url = os.environ.get("DATABASE_URL")
+database_url= database_url.replace("postgres", "postgresql", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app) ## Create DB object
